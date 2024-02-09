ec9e16daff62285fd91efc1921b95ecdb4411d99
Sofiia.Vynnytska <sofiia.vynnytska@igt.com>
2024-02-09 22:41:46 +0200

    OTB-2 Created the draft for online-ticket-booking

---
+++ otb-application/src/main/java/duikt/practice/otb/OtbApplication.java
@@ -0,0 +1,13 @@
+package duikt.practice.otb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtbApplication.class, args);
+    }
+
+}
