d768b31351ce926dd3ac44c1b9cfe00d064a094c
OleksandMatv <154979071+OleksandMatv@users.noreply.github.com>
2024-02-14 23:33:34 +0200

    Otb 7 get familiar with liquibase (#7)
    * otb-7 get familiar with liquibase and create first migration
    
    * otb-7 get familiar with liquibase and create first migration
    
    * Added otb-application module to the build
    
    * created new maven project for otb-doc, created setup-mysql.sh file for creation an Docker container and alternative file for it!
    
    * get one user
    
    * reverting force push of get_user.puml to develop
    
    * created uml diagram for user registration
    
    * Create sequence diagram for getting a list of users
    
    * otb-7 get familiar with liquibase and create first migration
    
    * reverting force push of get_user.puml to develop
    
    * created uml diagram for user registration
    
    * otb-7 get familiar with liquibase and create first migration
    
    * otb-7 get familiar with liquibase and create first migration
    
    * otb-7 get familiar with liquibase and create first migration
    
    * otb-7 get familiar with liquibase and create first migration
    
    * otb-7 get familiar with liquibase and create first migration
    
    otb-7 get familiar with liquibase and create first migration
    
    otb-7 get familiar with liquibase and create first migration
    
    otb-7 get familiar with liquibase and create first migration
    
    ---------
    
    Co-authored-by: Sofiia.Vynnytska <“sofiia.vynnytska@igt.com”>
    Co-authored-by: maksym_korniev <maksimkarulet8@gmail.com>
    Co-authored-by: huttonbern <huttonbernit@gmail.com>
    Co-authored-by: Ppllupold <kotnazar2005@gmail.com>
    Co-authored-by: Daryna Savytska <savytskadaryna1412@gmail.com>

---
+++ otb-doc/src/test/java/duikt/practice/otb/otbdoc/OtbDocApplicationTests.java
@@ -0,0 +1,13 @@
+package duikt.practice.otb.otbdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtbDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
